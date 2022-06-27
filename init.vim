f4443f8333da77279ae38e560776e8a7f03af183
jonathan <jonathan.montgo@gmail.com>
2022-06-27 15:18:19 -0500

    No super tab, Add file completion

--- init.vim
+++ init.vim
@@ -53,10 +53,11 @@ Plug 'hrsh7th/cmp-buffer'
 Plug 'hrsh7th/cmp-path'
 Plug 'hrsh7th/cmp-cmdline'
 Plug 'hrsh7th/nvim-cmp'
+"
 " For vsnip users.
 Plug 'hrsh7th/cmp-vsnip'
 Plug 'hrsh7th/vim-vsnip'
-Plug 'ervandew/supertab'
+" Plug 'ervandew/supertab'
 " Auto format on save
 
 call plug#end()
--- lua/lsp/completion.lua
+++ lua/lsp/completion.lua
@@ -34,6 +34,7 @@ cmp.setup({
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'vsnip' }, -- For vsnip users.
+    { name = 'path' } -- Path completion I miss you!
     -- { name = 'luasnip' }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
     -- { name = 'snippy' }, -- For snippy users.
