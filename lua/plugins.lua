b0cd7db0aeabd6c1a25af88b21abfaa43a760bda
jonathan <jonathan.montgo@gmail.com>
2022-07-16 15:02:18 -0500

    Don't need this line

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,3 @@
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function()
   -- Manage packer with packer... neat trick
   use 'wbthomason/packer.nvim'
