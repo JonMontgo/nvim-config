f20deb50189eba35481e52fd8470d510b4246ed3
Jonathan Montgomery <jonathan.montgo@gmail.com>
2024-04-30 09:27:01 -0500

    terraform lsp added

--- lua/lsp/nonels.lua
+++ lua/lsp/nonels.lua
@@ -13,11 +13,13 @@ null_ls.setup({
 		null_ls.builtins.formatting.isort,
 		null_ls.builtins.formatting.eslint,
 		null_ls.builtins.formatting.remark,
+		null_ls.builtins.formatting.terraform_fmt,
 
 		-- Completions
 		null_ls.builtins.completion.spell,
 
 		-- Diagnostics
 		null_ls.builtins.diagnostics.markdownlint,
+		-- null_ls.builtins.diagnostics.terraform_validate,
 	},
 })
