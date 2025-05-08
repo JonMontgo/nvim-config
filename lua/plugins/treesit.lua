0e2fc3276af81072cd494cd7a5af917afa1ef902
Jonathan Montgomery <jonathan.montgo@gmail.com>
2025-05-08 10:34:40 -0500

    Fix tree sitter

--- lua/plugins/treesit.lua
+++ lua/plugins/treesit.lua
@@ -1,35 +1,39 @@
 return {
   {
+    'nvim-treesitter/playground',
+    dependencies = {'nvim-treesitter/nvim-treesitter'}
+  },
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     dependencies = { 'nvim-treesitter/playground' },
-    opts = {
-      ensure_installed = {
-        "c",
-        "lua",
-        "rust",
-        "go",
-        "python",
-        "tsx",
-        "typescript",
-        "dockerfile",
-        "json",
-        "json5",
-        "yaml",
-        "markdown",
-        "markdown_inline"
-      },
-      auto_install=true,
-      highlight = { 
-        enable = true 
-      },
-      indent = {
-        enable = true,
-      }
-    },
     config = function ()
       vim.opt.foldmethod='expr'
       vim.opt.foldexpr='nvim_treesitter#foldexpr()'
+      require 'nvim-treesitter.configs'.setup {
+        ensure_installed = {
+          "c",
+          "lua",
+          "rust",
+          "go",
+          "python",
+          "tsx",
+          "typescript",
+          "dockerfile",
+          "json",
+          "json5",
+          "yaml",
+          "markdown",
+          "markdown_inline"
+        },
+        auto_install=true,
+        highlight = {
+          enable = true
+        },
+        indent = {
+          enable = true,
+        }
+      }
     end
   },
 }
