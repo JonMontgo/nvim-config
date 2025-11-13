d1f483019f06555b974f06d80736e3971713f4dd
Jonathan Montgomery <jonathan.montgo@gmail.com>
2025-11-13 12:31:26 -0600

    add mcphub, update avante config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,13 +5,26 @@ return {
     version = false, -- Never set this value to "*"! Never!
     build = "make",
     opts = {
-      provider = "openai",
+      provider = "gemini",
+      system_prompt = function ()
+        local hub = require("mcphub").get_hub_instance()
+        return hub and hub:get_active_servers_prompt() or ""
+      end,
+      custom_tools = function ()
+        return {
+          require("mcphub.extensions.avante").mcp_tool()
+        }
+      end,
       providers = {
         ollama = {
           model = "deepseek-r1:14b",
         },
         gemini = {
-          model = "gemini-2.5-flash-lite-preview-06-17",
+          model = "gemini-2.5-flash",
+          extra_requrest_body = {
+            temperature = 0.75,
+            max_tokens = 1000000
+          }
         },
         openai = {
           endpoint = "https://api.openai.com/v1",
@@ -24,7 +37,17 @@ return {
             --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
           },
         }
-      }
+      },
+      acp_providers = {
+        ["gemini-cli"] = {
+          command = "gemini",
+          args = { "--experimental-acp" },
+          env = {
+            NODE_NO_WARNINGS = "1",
+            GEMINI_API_KEY = os.getenv("GEMINI_API_KEY"),
+          },
+        },
+      },
     },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
@@ -54,4 +77,3 @@ return {
     },
   },
 }
-
